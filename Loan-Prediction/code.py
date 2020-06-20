c8da26325e65f62955b72b6b4449a5cac7ac00d3
umeshpal93 <umeshpal93@users.noreply.github.com>
2020-06-21 02:41:17 +0530

    Code file created.

---
+++ Loan-Prediction/code.py
@@ -0,0 +1,78 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv(path)
+
+loan_status = data['Loan_Status'].value_counts()
+
+#fig = plt.figure()
+plt.bar(loan_status.index , loan_status)
+plt.show()
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area', 'Loan_Status']).size().unstack()
+print(property_and_loan)
+property_and_loan.plot(kind = 'bar', stacked=False, figsize=(15,10))
+
+plt.xlabel('Property Area')
+plt.ylabel('Loan status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+# --------------
+#Code starts here
+education_and_loan = data.groupby(['Education', 'Loan_Status']).size().unstack()
+print(property_and_loan)
+education_and_loan.plot(kind = 'bar', stacked=False, figsize=(15,10))
+
+plt.xlabel('Education Status')
+plt.ylabel('Loan status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+# --------------
+#Code starts here
+graduate = data[data['Education'] == 'Graduate']
+
+not_graduate = data[data['Education'] == 'Not Graduate']
+
+
+graduate['LoanAmount'].plot(kind = 'density', label='Graduate')
+
+not_graduate['LoanAmount'].plot(kind = 'density', label='Not Graduate')
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig ,(ax_1,ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
+
+ax_1.scatter(data['ApplicantIncome'],data["LoanAmount"])
+ax_1.set(title='Applicant Income')
+
+ax_2.scatter(data['CoapplicantIncome'],data["LoanAmount"])
+ax_2.set(title='Coapplicant Income')
+
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data["LoanAmount"])
+ax_3.set(title='Total Income')
+
+
